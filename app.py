c4c1149550d8602c103a190496f60e9dd2c32eef
Victor Tolmachev <tolmachev.2406@gmail.com>
2020-12-07 02:24:56 +0300

    Add query profiling

--- app.py
+++ app.py
@@ -1,24 +1,45 @@
 import streamlit as st
 import os
+import time
 from src.search import SearchClient
 
 
 @st.cache(allow_output_mutation=True)
-def load_search_client():
-    return SearchClient(
-        os.path.join("data", "inverted_index.pkl"), os.path.join("data", "news.csv")
-    )
+def load_search_client(compression_type="uncompressed"):
+    if compression_type == "uncompressed":
+        return SearchClient(
+            os.path.join("data", "inverted_index.pkl"), os.path.join("data", "news.csv")
+        )
+    elif compression_type == "elias_gamma":
+        return SearchClient(
+            os.path.join("data", "inverted_index_gamma_compressed.pkl"),
+            os.path.join("data", "news.csv"),
+        )
+    elif compression_type == "elias_delta":
+        return SearchClient(
+            os.path.join("data", "inverted_index_delta_compressed.pkl"),
+            os.path.join("data", "news.csv"),
+        )
+    else:
+        raise Exception(f"Wrong compression type: {compression_type}")
 
 
 def main():
-    search_client = load_search_client()
+    st.sidebar.title("Compression")
+    compression_type = st.sidebar.radio(
+        "", options=("uncompressed", "elias_gamma", "elias_delta")
+    )
+    search_client = load_search_client(compression_type=compression_type)
 
     st.sidebar.title("Search")
     query = st.sidebar.text_input("Query")
     run_button = st.sidebar.button("Run")
 
     if run_button:
+        start_time = time.perf_counter()
         result = search_client.search(query)
+        elapsed_time = time.perf_counter() - start_time
+        st.sidebar.text(f"Elapsed time: {elapsed_time:.5f}")
         st.title("Found TOP-10")
         if result:
             st.write(result)
--- cli.py
+++ cli.py
@@ -1,23 +1,28 @@
 import os
 
-from src.id_service import IDService
 from src.inverted_index import InvertedIndex
 
 
 def main():
     collection_file = os.path.join("data", "news.csv")
     index_file = os.path.join("data", "inverted_index.pkl")
+    index_gamma_file = os.path.join("data", "inverted_index_gamma_compressed.pkl")
+    index_delta_file = os.path.join("data", "inverted_index_delta_compressed.pkl")
 
     if not os.path.exists(index_file):
         inverted_index = InvertedIndex()
         inverted_index.create_index(collection_file)
         inverted_index.save(index_file)
 
-    inverted_index = InvertedIndex.load(index_file)
+    if not os.path.exists(index_gamma_file):
+        inverted_index = InvertedIndex.load(index_file)
+        inverted_index.compress(compress_method="elias_gamma")
+        inverted_index.save(index_gamma_file)
 
-    id_service = IDService(inverted_index)
-    id_service.create_post_ids()
-    id_service.create_word_ids()
+    if not os.path.exists(index_delta_file):
+        inverted_index = InvertedIndex.load(index_file)
+        inverted_index.compress(compress_method="elias_delta")
+        inverted_index.save(index_delta_file)
 
 
 if __name__ == "__main__":
