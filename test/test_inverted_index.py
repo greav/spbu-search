275787f9e8b4fd27a3e9040d9a2e4aee186b5e81
Lada <lada.astashenkova@yandex.ru>
2020-12-08 03:37:21 +0300

    Add one more test for inverted_index (exception "Index already exists")

--- test/test_inverted_index.py
+++ test/test_inverted_index.py
@@ -11,20 +11,23 @@ class TestInvertedIndex(unittest.TestCase):
     # Test for compress()
     def test_compress(self):
         self.assertRaises(Exception, self.invertedIndex.compress, "elias")
+        print("#1: OK")
 
     # Tests for save()
 
     # Test for Exception("You should create index before saving")
     def test_save_without_creating(self):
-        index_file = os.path.join("data", "inverted_index.pkl")
+        index_file = os.path.join("test_data", "inverted_index.pkl")
         self.assertRaises(Exception, self.invertedIndex.save, index_file)
+        print("#2: OK")
 
-    # # Test for Exception("Index already exists")
-    # def test_save_for_existing(self):
-    #     collection_file = os.path.join("data", "news.csv")
-    #     index_file = os.path.join("data", "inverted_index.pkl")
-    #     self.invertedIndex.create_index(collection_file)
-    #     self.assertRaises(Exception, self.invertedIndex.save, index_file)
+    # Test for Exception("Index already exists")
+    def test_save_for_existing(self):
+        collection_file = os.path.join("test_data", "news.csv")
+        index_file = os.path.join("test_data", "inverted_index.pkl")
+        self.invertedIndex.create_index(collection_file)
+        self.assertRaises(Exception, self.invertedIndex.save, index_file)
+        print("#3: OK")
 
 
 if __name__ == "__main__":
